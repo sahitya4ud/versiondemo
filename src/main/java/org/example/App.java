4de9b7ce9f210014e83e6111407bdf84842f8805
sahitya4ud <sahitya4ud@gmail.com>
2024-09-21 11:21:10 -0500

    changes in developer3
    (cherry picked from commit 6ef9f8caca2235aeeea3e11ab73922939e4aa443)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,10 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("bug fixed");
-        System.out.println("changes in developer 1");
-        System.out.println("advanced learning");
         System.out.println("job done");
-        System.out.println("hello");
     }
 }
